d05d36003759440281c76360f78ab76633c79512
Kalepson <kalepson@gmail.com>
2022-01-27 13:47:05 +0200

    Modificari html , css , js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,40 +1,31 @@
-const button_html = document.querySelector(".html")
-const button_css = document.querySelector(".css")
-const button_js = document.querySelector(".js")
+const button_html = document.querySelector(".button_html")
+const button_css = document.querySelector(".button_css")
+const button_js = document.querySelector(".button_js")
+const slide_container = document.querySelector(".slide_container")
 const body = document.querySelector("body")
-const slideLine = document.querySelector(".slide-line")
-let poziti = 0;
 
-button_html.addEventListener("click" ,  () =>{
+button_css.addEventListener("click", () => {
+    button_css.classList.toggle("button_active")
+    body.style.backgroundColor = "#9392E2FF"
+    button_html.className = "button_passive"
+    slide_container.style.marginLeft = "0px"
+})
+
+button_html.addEventListener("click", () => {
+    button_html.classList.toggle("button_active")
     body.style.backgroundColor = "#EFADC3FF"
-    button_html.classList.toggle("html1")
-    button_js.className ="js"
-    poziti = poziti + 400;
-    if (poziti > 400){
-        poziti =0;
-    }
-    slideLine.style.right = -poziti + 'px';
+    button_js.className = "button_passive"
+    slide_container.style.marginLeft = "400px"
+
 })
 
-button_css.addEventListener("click" , () => {
-    body.style.backgroundColor = "#9C94E0FF"
-    button_css.classList.toggle("css1")
-    button_html.className ="html";
-    poziti = poziti + 400;
-    if (poziti > 800){
-        poziti =0;
-    }
-    slideLine.style.right = -poziti + 'px';
+button_js.addEventListener("click", () => {
+    button_js.classList.toggle("button_active")
+    body.style.backgroundColor = "#EDE34DFF"
+    button_css.className = "button_passive"
+    slide_container.style.marginLeft = "-800px"
 })
 
-button_js.addEventListener("click" ,  () =>{
-    body.style.backgroundColor = "#F5E54AFF"
-    button_js.classList.toggle("js1")
-    button_css.className = "css";
-    poziti = poziti + 400;
-    if (poziti > 800){
-        poziti =0;
-    }
-    slideLine.style.left = -poziti + 'px';
-})
+
+
 
