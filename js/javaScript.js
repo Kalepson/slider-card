79ad18c6b02ba93aec2b51ed26090217bd6ac54f
Kalepson <kalepson@gmail.com>
2022-01-29 22:15:15 +0200

    numai schimb nimic

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,31 +1,27 @@
-const button_html = document.querySelector(".button_html")
-const button_css = document.querySelector(".button_css")
-const button_js = document.querySelector(".button_js")
 const slide_container = document.querySelector(".slide_content")
 const body = document.querySelector("body")
+let buttons = document.querySelectorAll('.btn');
+body.style.backgroundColor = "#EFADC3FF"
 
-button_css.addEventListener("click", () => {
-    button_css.className = "button_active"
-    body.style.backgroundColor = "#9392E2FF"
-    slide_container.style.marginLeft = "0px"
-    button_html.className = "button_passive"
-    button_js.className = "button_passive"
-})
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
 
-button_html.addEventListener("click", () => {
-    button_html.className = "button_active"
-    button_html.className = "button_active";
+buttons[0].addEventListener("click", () => {
     body.style.backgroundColor = "#EFADC3FF"
-    button_js.className = "button_passive"
-    button_css.className = "button_passive"
     slide_container.style.marginLeft = "600px"
 })
 
-button_js.addEventListener("click", () => {
-    button_js.className = "button_active"
+buttons[1].addEventListener("click", () => {
+    body.style.backgroundColor = "#9392E2FF"
+    slide_container.style.marginLeft = "0px"
+})
+
+buttons[2].addEventListener("click", () => {
     body.style.backgroundColor = "#EDE34DFF"
-    button_css.className = "button_passive"
-    button_html.className = "button_passive"
     slide_container.style.marginLeft = "-800px"
 })
 
